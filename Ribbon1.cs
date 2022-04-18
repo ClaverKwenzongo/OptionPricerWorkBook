372f82a22590532e2b953fbde8ae2046c466dc29
ClaverKwenzongo <ClaverKwenzongo@DESKTOP-H3HP192>
2022-04-18 14:46:38 +0200

    Add project files.

---
+++ Ribbon1.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OfficeOpenXml;
+using System.Globalization;
+using System.Diagnostics;
+
+namespace OptionPricerWorkBook
+{
+    public partial class Ribbon1
+    {
+        int row_start = 5;
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+            Globals.Sheet4.Cells[row_start+2, 3].Value = "Share Name";
+            Globals.Sheet4.Cells[row_start+3, 3].Value = "Start Date";
+            Globals.Sheet4.Cells[row_start + 4, 3].Value = "Maturity date";
+            Globals.Sheet4.Cells[row_start+5, 3].Value = "Position";
+            Globals.Sheet4.Cells[row_start+6, 3].Value = "Option Type";
+            Globals.Sheet4.Cells[row_start+7, 3].Value = "Amount";
+
+
+        }
+
+        static int getCol_Increase()
+        {
+            int col_count = 1;
+            int col3 = 2;
+            while (string.IsNullOrWhiteSpace(Globals.Sheet2.Cells[2,col3].Value?.ToString()) == false)
+            {
+                col_count++;
+                col3++;
+            }
+
+            return col_count;
+        }
+
+        static double tenor( string start, string end)
+        {
+            CultureInfo culture = new CultureInfo("es-ES");
+
+            DateTime end_date = DateTime.Parse(end, culture);
+            DateTime start_date = DateTime.Parse(start, culture);
+
+            TimeSpan interval = end_date - start_date;
+            double days = (double)interval.TotalDays;
+
+            return days;
+        }
+
+            private void testBtn_Click(object sender, RibbonControlEventArgs e)
+        {
+            getSharePrice getShare = new getSharePrice();
+            getImpliedVol getVol = new getImpliedVol();
+            getYield getDividentYield = new getYield();
+
+            string myStartDate = Globals.Sheet4.Cells[row_start+3,4].Value.ToString();
+            Debug.WriteLine(myStartDate);
+            string user_share = Globals.Sheet4.Cells[row_start+2,4].Value;
+            double s0 = getShare._getSharePrice(myStartDate, user_share.ToUpper());
+
+            //Globals.Sheet4.Cells[3,4].Value = 
+            int col_inc = getCol_Increase();
+            double Tenor = tenor(Globals.Sheet4.Cells[row_start+3,4].Value.ToString(),Globals.Sheet4.Cells[row_start+4,4].Value.ToString());
+            double impl_Vol = getVol.getImpl_Vol(Tenor, myStartDate, col_inc, user_share.ToUpper());
+
+            double div_yield = getDividentYield.getDiv_Yield(Tenor, myStartDate, col_inc, user_share.ToUpper());
+
+            //Globals.Sheet4.Cells[3, 4].Value = div_yield;
+
+            //using (ExcelWorksheet ws = Globals.Sheet2 as ExcelWorksheet)
+            //{
+            //}
+            //int div_start_col = getStartCol(Globals.Sheet3, user_share.ToUpper());
+        }
+    }
+}
---
+++ getSharePrice.cs
@@ -0,0 +1,66 @@
+﻿using OfficeOpenXml;
+using System.Globalization;
+using System;
+using System.Diagnostics;
+
+namespace OptionPricerWorkBook
+{
+    public class getSharePrice
+    {
+        static DateTime getDate(string _date)
+        {
+            CultureInfo cultureInfo = new CultureInfo("en-US");
+            DateTime wsDate = DateTime.Parse(_date, cultureInfo);
+
+            return wsDate;
+        }
+
+
+        public double _getSharePrice(string date, string shareName)
+        {
+            double share_price = 0;
+
+            int date_row = 0;
+            int row = 3;
+            while (string.IsNullOrWhiteSpace(Globals.Sheet1.Cells[row, 1].Value?.ToString()) == false)
+            {
+                DateTime ws_date = getDate(Globals.Sheet1.Cells[row, 1].Value.ToString());
+                Debug.WriteLine(ws_date);
+                if (ws_date.ToString("dd/MM/yyyy") == date)
+                {
+                    date_row = row;
+                    break;
+                }
+                row++;
+            }
+
+
+            int col = 2;
+            int share_col = 0;
+            while (string.IsNullOrWhiteSpace(Globals.Sheet1.Cells[2, col].Value?.ToString()) == false)
+            {
+                if (Globals.Sheet1.Cells[2, col].Value == shareName)
+                {
+                    share_col = col;
+                    break;
+                }
+
+                col++;
+            }
+
+            share_price = Globals.Sheet1.Cells[date_row, share_col].Value;
+
+            return share_price;
+        }
+        //public double GetSharePrice(ExcelWorkbook ws, string shareName, string start_date)
+        //{
+        //    double share_price = 0;
+
+        //    int col = 1;
+        //    while (string.IsNullOrEmpty(ws.Cells[2, col].Value.ToString()))
+
+        //        return share_price;
+        //}
+
+    }
+}
